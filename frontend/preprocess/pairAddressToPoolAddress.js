88e43d891f62775e102912b6029852e60ca55905
Craigton Lian <108467437+craigtonlian@users.noreply.github.com>
2022-12-17 18:02:33 +0800

    Update pairAddressToPoolAddress.js

--- frontend/preprocess/pairAddressToPoolAddress.js
+++ frontend/preprocess/pairAddressToPoolAddress.js
@@ -1,5 +1,6 @@
-const { tokenPairAddress } = require("./tokenPairAddress");
+const { tokenPairAddressTest } = require("./tokenPairAddressTest");
 const { tokenPair } = require("./tokenPair");
+const { tokenDS } = require("./tokenDS");
 const { contractAbi } = require("./contractAbi");
 const ethers = require("ethers");
 
@@ -8,7 +9,7 @@ const factoryAddress = "0x1F98431c8aD98523631AE4a59f267346ea31F984";
 const factoryAbi = contractAbi;
 const providerUrl =
   "https://mainnet.infura.io/v3/89cd0af4ef3a4501806c55ec05d5a7d7";
-const fee = 100; // 0.3%. This is measured in hundredths of a bip (100, 500, 3000)
+// const feeTier = 100; // (0.01% = 100, 0.05% = 500, 0.30% = 3000, 1.00% = 10000)
 
 const provider = new ethers.providers.JsonRpcProvider(providerUrl);
 const factoryContract = new ethers.Contract(
@@ -22,32 +23,38 @@ async function getPoolAddresses(pairs) {
 
   for (const pair of pairs) {
     const [token0, token1] = pair;
-    const index = pairs.indexOf(pair);
-    const tokenPairKey = tokenPair[index];
-    // console.log(tokenPairKey);
+    const ticker0 = Object.keys(tokenDS).find((key) =>
+      tokenDS[key].address.includes(token0)
+    );
+    const ticker1 = Object.keys(tokenDS).find((key) =>
+      tokenDS[key].address.includes(token1)
+    );
+    console.log(ticker0);
+    console.log(ticker1);
 
-    try {
+    for (const fee of [100, 500, 3000, 10000]) {
       const poolAddress = await factoryContract.functions.getPool(
         token0,
         token1,
         fee
       );
 
-      const key0 = tokenPairKey[0];
-      const key1 = tokenPairKey[1];
-
-      if (!(key0 in poolAddressDS)) {
-        poolAddressDS[key0] = {};
+      if (poolAddress[0] === ethers.constants.AddressZero) {
+        continue;
+      } else {
+        if (!(ticker0 in poolAddressDS)) {
+          poolAddressDS[ticker0] = {};
+        }
+        if (!(ticker1 in poolAddressDS[ticker0])) {
+          poolAddressDS[ticker0][ticker1] = {};
+        }
+        poolAddressDS[ticker0][ticker1][fee] = poolAddress[0];
+        console.log(poolAddressDS);
       }
-      poolAddressDS[key0][key1] = poolAddress;
-      console.log(poolAddressDS);
-    } catch (error) {
-      console.error(error);
     }
   }
-
   return poolAddressDS;
 }
-getPoolAddresses(tokenPairAddress).then((poolAddressDS) =>
+getPoolAddresses(tokenPairAddressTest).then((poolAddressDS) =>
   console.log(poolAddressDS)
 );
