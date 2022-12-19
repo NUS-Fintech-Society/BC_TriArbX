34aacba2cdacf30ca951c7ad9806219da0ae39e9
Justin <justinleng_@hotmail.com>
2022-12-19 20:57:57 +0800

    Revert "Merge branch 'feature/preprocessing-token-list' into feature/query-uniswap-data-and-process"
    This reverts commit 5900581c012de46bff1f47042d6b5b822bd36673, reversing
    changes made to 88e43d891f62775e102912b6029852e60ca55905.

--- frontend/utils/uniswap/abi.tsx
+++
@@ -1,8 +0,0 @@
-export const poolImmutablesAbi = [
-	"function factory() external view returns (address)",
-	"function token0() external view returns (address)",
-	"function token1() external view returns (address)",
-	"function fee() external view returns (uint24)",
-	"function tickSpacing() external view returns (int24)",
-	"function maxLiquidityPerTick() external view returns (uint128)",
-];
